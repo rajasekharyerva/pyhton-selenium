11fe52bb23d95cebe3e002e04ed60648baae9ec0
Rajasekhar Reddy Yerva <rajasekharreddyyerva@Rajasekhars-MacBook-Pro.local>
2024-11-03 21:42:52 +0530

    added workflow

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 import pytest
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def browser():
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome(options=options)
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
