b8f71879da848c425cca8757d929c0c87c731128
Rajasekhar Reddy Yerva <rajasekharreddyyerva@Rajasekhars-MacBook-Pro.local>
2024-11-04 03:12:50 +0530

    test headless

--- conftest.py
+++ conftest.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
